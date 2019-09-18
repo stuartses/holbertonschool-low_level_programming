692aac1bee9a9f6fb7aa42c5af745f39a43d7ed2
stuartses <stuart.ses@hotmail.com>
2019-09-18 19:48:33 +0000

    update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-
 /*
  * 4. Hello, puts
  * This Will display the text and 0 output
  */
+#include <stdio.h>
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
