54008d52eec4b776466f35e154533de106a9b706
stuartses <stuart.ses@hotmail.com>
2019-09-18 20:26:15 +0000

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - 5. Hello, printf
+ * Prints a text with printf
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
