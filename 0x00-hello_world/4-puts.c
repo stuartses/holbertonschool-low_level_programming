8fbb1f0ef4114655d87c61e8ab604c9353ff56a1
stuartses <stuart.ses@hotmail.com>
2019-09-18 19:27:25 +0000

    update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	getchar();
-	return 0;
+	return (0);
 }
