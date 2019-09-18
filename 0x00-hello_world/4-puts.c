df7a41cad144eb0ed27fd7b78aabbdefa18ab5c7
stuartses <stuart.ses@hotmail.com>
2019-09-18 19:37:08 +0000

    Update 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 /*
  * 4. Hello, puts
+ * This Will display the text and 0 output
  */
 
 #include <stdio.h>
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	getchar();
 	return (0);
 }
