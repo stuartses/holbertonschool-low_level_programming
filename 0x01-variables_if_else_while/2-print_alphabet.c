6f66d86cb43dfbeba648973b61ee2cef0f83526e
stuartses <stuart.ses@hotmail.com>
2019-09-19 19:04:54 +0000

    2. I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+/**
+ * main - 2. I sometimes suffer from insomnia. And when I can't fall asleep,
+ * I play what I call the alphabet
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar (x);
+	putchar('\n');
+	return (0);
+}
