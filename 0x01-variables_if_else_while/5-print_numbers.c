8dce7467796de0bbebd02e699187eafcac01e1c6
stuartses <stuart.ses@hotmail.com>
2019-09-19 19:47:20 +0000

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+/**
+ * main - 5. Numbers
+ * print numbers from 0 to 9
+ * Return: 0
+ */
+#include <stdio.h>
+int main(void)
+{
+	int x;
+
+	/* print numbers from 0 to 9 */
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+
+	printf("\n");
+	return (0);
+}
