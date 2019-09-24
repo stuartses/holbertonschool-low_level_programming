f60c740a35ea73567c699c92f676aeebe143e7cf
stuartses <stuart.ses@hotmail.com>
2019-09-24 16:44:55 +0000

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _islower - Say if a char is lowercase or uppercase
+ * @c: the input
+ *
+ * Description: if the input is lowercase or uppercase prints "1"
+ *              otherwise print "0"
+ * Return: "1" or "0"
+ */
+
+int _isalpha(int c)
+{
+	int y;
+
+	if (c >= 97 && c <= 122)
+		y = 1;
+	else if (c >= 65 && c <= 90)
+		y = 1;
+	else
+		y = 0;
+
+	return (y);
+}
