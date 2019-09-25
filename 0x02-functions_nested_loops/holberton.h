2a26ea2020195ad296080f577ce91e5595a34794
stuartses <stuart.ses@hotmail.com>
2019-09-25 15:11:53 +0000

    update Holberton.h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
 /*  Declaration of functions */
 int _putchar(char c);
 int add(int, int);
+=======
+/*
+  Declaration of functions
+ */
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int x);
+int print_last_digit(int x);
+void jack_bauer(void);
+
+#endif /* HOLBERTON_H */
+>>>>>>> update Holberton.h
