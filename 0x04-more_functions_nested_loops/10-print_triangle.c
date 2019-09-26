eb24085714a64bb6656c6ec55cd829e302146ed5
stuartses <stuart.ses@hotmail.com>
2019-09-26 21:43:28 +0000

    update 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,11 +11,11 @@ void print_triangle(int size)
 {
 	int i, j, punter;
 	/* punter */
-	punter = size;
+	punter = size - 1;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j <= size; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (j >= punter)
 				_putchar('#');
