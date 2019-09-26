bb3246a787665f8b31548404dc2e33a9bbec08ac
stuartses <stuart.ses@hotmail.com>
2019-09-26 17:58:43 +0000

    update 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,19 +9,15 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14;  j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + 48);
-			}
-			else
+			if (j > 10)
 			{
 				_putchar((j / 10) + 48);
-				_putchar((j % 10) + 48);
 			}
+			_putchar((j % 10) + 48);
 		}
 		_putchar('\n');
 	}
