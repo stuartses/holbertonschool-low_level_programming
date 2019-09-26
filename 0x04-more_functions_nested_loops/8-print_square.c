748848f7eddf20f1836f083fd2ffadd7eaff2208
stuartses <stuart.ses@hotmail.com>
2019-09-26 19:03:36 +0000

    8. You are so much sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_square - print a square in terminal
+ * @size: the size of square
+ *
+ * Description: Square maded with "#"
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar ('\n');
+		}
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+}
