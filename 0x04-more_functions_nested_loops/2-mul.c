fce5c74d9eb806025abb09abc621e0437295d892
stuartses <stuart.ses@hotmail.com>
2019-09-26 17:43:53 +0000

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two integers
+ * @a: the firt number
+ * @b: the second number
+ *
+ * Description: Multiplieas a * b
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	int y;
+
+	y = a * b;
+	return (y);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -5,5 +5,6 @@ int _isupper(int c);
 int _isdigit(int c);
 void print_numbers(void);
 void print_most_numbers(void);
+int mul(int a, int b);
 
 #endif /* HOLBERTON_H */
