9103d523daccc5fc6dbbb41c53b608cdff1988bf
stuartses <stuart.ses@hotmail.com>
2019-09-26 17:04:42 +0000

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+void print_numbers(void);
+void print_most_numbers(void);
 
 #endif /* HOLBERTON_H */
