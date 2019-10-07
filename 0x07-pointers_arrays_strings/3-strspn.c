24af41941a7283adeadadfeb0d4ddc521327fe7c
stuartses <stuart.ses@hotmail.com>
2019-10-07 21:35:02 +0000

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strspn - 3. strspn
+ * @s: input string
+ * @accept: string wanted
+ *
+ * Description: function that locates a character in a string
+ * Return: position of last match
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, found = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found == 0)
+			break;
+		else
+			found = 0;
+	}
+	return (i);
+}
