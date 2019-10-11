72f36593d777159339a350dbc7bb731de73f3de2
stuartses <stuart.ses@hotmail.com>
2019-10-11 03:12:56 +0000

    7. They say we only use a fraction of our brain's true potential

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,61 @@
+#include "holberton.h"
+#include <stdio.h>
+int pali_look(char *str, int c1, int c2, int is_par);
+
+/**
+ * pali_look - find if is palidrome
+ * @str: input str;
+ * @c1: position initial
+ * @c2: psition final
+ * @is_par: if lenght of string is par => 1
+ *
+ * Description: Determine if is palindrome
+ * Return: 1 or 0
+ */
+
+int pali_look(char *str, int c1, int c2, int is_par)
+{
+	int pal = 0;
+
+	if (is_par == 0 && c1 == c2)
+		return (1);
+
+	if (is_par == 1 && (c1 + 1) == c2)
+		return (1);
+
+	if (str[c1] == str[c2])
+		pal = pali_look(str, c1 + 1, c2 - 1, is_par);
+	else
+		return (0);
+
+	return (pal);
+}
+
+
+/**
+ * is_palindrome -  determine palindrome
+ * @s: input string
+ *
+ * Description:  returns 1 if is palindrome
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+	int is_pali, len = 0, len_par;
+	char *strg = s;
+
+	while (s[len] != '\0')
+		len++;
+
+	if ((len % 2) == 0)
+		len_par = 1;
+	else
+		len_par = 0;
+
+	len = len - 1;
+
+	is_pali = pali_look(strg, 0, len, len_par);
+
+	return (is_pali);
+}
