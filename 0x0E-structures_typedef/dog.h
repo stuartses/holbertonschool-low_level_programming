cfb3dc7f5fd40274c478f31a9e16deb912b48f7f
stuartses <stuart.ses@hotmail.com>
2019-10-21 19:12:12 +0000

    update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - dog properties
+ * struct dog - get properties of Dog
+ * @name: name of Dog
+ * @age: age of dog
+ * @owner: owner of dog
  *
- * Description: define properties for dog
+ * Description: structure that defines properties of Dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif /* DOG_H */
