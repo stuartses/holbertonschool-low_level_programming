b28a694db928e09165ad1abc7e17cfedf3e3f0ee
stuartses <stuart.ses@hotmail.com>
2019-10-23 17:51:26 +0000

    update 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdlib.h>
 /**
  * int_index - searches for the first element different to zero
  * @array: input array
@@ -14,6 +14,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int om, i;
 
 	if (size <= 0)
+		return (-1);
+
+	if (cmp == NULL || array == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
