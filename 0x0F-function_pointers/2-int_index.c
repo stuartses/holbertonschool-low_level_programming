ab902fd80213bbba4980e936e52b59e1549ba0f2
stuartses <stuart.ses@hotmail.com>
2019-10-23 16:30:06 +0000

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for the first element different to zero
+ * @array: input array
+ * @size: number of elements in array
+ * @cmp: function to executes comparation
+ *
+ * Description: function that searches for an integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int om, i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		om = (*cmp)(array[i]);
+		if (om != 0)
+			return (i);
+	}
+	return (-1);
+}
