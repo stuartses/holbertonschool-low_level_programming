5d563594a835a63fc61ec6ce9498e4c421ae212a
stuartses <stuart.ses@hotmail.com>
2019-11-04 17:53:24 +0000

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint - free all elements of a add last node list
+ * @head: input list
+ *
+ * Description: free all memory of las node list
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp, *temp_next;
+
+	temp = head;
+	while (temp->next != NULL)
+	{
+		temp_next = temp->next;
+		free(temp);
+		temp = temp_next;
+	}
+	free(temp->next);
+	free(temp);
+	head = NULL;
+}
