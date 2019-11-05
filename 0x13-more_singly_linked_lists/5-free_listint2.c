478bbfede5ee4f843084bdb4b1c0e9759b9f2697
stuartses <stuart.ses@hotmail.com>
2019-11-05 13:52:53 +0000

    5. Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * free_listint2 - free all elements of a add last node list
+ * @head: input list
+ *
+ * Description: free all memory of las node list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp, *temp_next;
+
+	if (*head == NULL)
+		return;
+
+	temp = *head;
+	while (temp->next != NULL)
+	{
+		temp_next = temp->next;
+		free(temp);
+		temp = temp_next;
+	}
+	free(temp->next);
+	free(temp);
+	*head = NULL;
+}
