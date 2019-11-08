2ec1f6156eb8fa5d66a1396d521cd2d19967a4dc
stuartses <stuart.ses@hotmail.com>
2019-11-08 23:08:06 +0000

    3. Change a bit in the index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * set_bit - change a bit to "1" in the index
+ * @n: decimal number in pointer
+ * @index: position in bits
+ *
+ * Description:  unction that sets the value of a bit to 1 at a given index
+ * Return: 1 if is sucessfull or -1 if fails
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int num_bits, mask = 1;
+	int new_number;
+
+	num_bits = (8 * sizeof(unsigned int)) - 1;
+
+	if (index > num_bits)
+		return (-1);
+
+	mask = mask << index;
+	new_number = mask | *n;
+	*n = new_number;
+	return (1);
+}
