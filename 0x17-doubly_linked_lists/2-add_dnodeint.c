6ca5dfb544511cb5237c9ddbd6e3ec8fcc16aaac
stuartses <stuart.ses@hotmail.com>
2019-12-15 15:52:22 +0000

    Fix in case when head is NULL

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	if (head == NULL)
+	if (*head == NULL)
 		new->next = NULL;
 	else
 		new->next = *head;
