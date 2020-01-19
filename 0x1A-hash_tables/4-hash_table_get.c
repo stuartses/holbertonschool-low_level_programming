973fe17e17750a09891de4de8eb837b0840775cc
stuartses <stuart.ses@hotmail.com>
2020-01-19 22:16:54 +0000

    Task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - get value at key in a Hash Table
+ * @ht: input hash table
+ * @key: input key
+ *
+ * Description: gets the value at kay
+ * Return: value or NULL if Fails
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int hash_index;
+	hash_node_t *hash_list;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	hash_index = key_index((unsigned char *)key, ht->size);
+	hash_list = ht->array[hash_index];
+
+	while (hash_list != NULL)
+	{
+		if (strcmp(hash_list->key, key) == 0)
+			return (hash_list->value);
+	}
+
+	return (NULL);
+}
