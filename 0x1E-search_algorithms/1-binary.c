5e67639b192b72402bbe6f5583aac9078eb9ced0
stuartses <stuart.ses@hotmail.com>
2020-04-14 16:16:48 -0500

    Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,54 @@
+#include "search_algos.h"
+
+/**
+ * _print_binaryserach - print element of array by binary search
+ * @array: array of integers
+ * @L: left limit integer
+ * @R: right limit integer
+ *
+ * Description: print each element of an array in binary search limits
+ * Return: void
+ */
+void _print_binaryserach(int *array, int L, int R)
+{
+	int i;
+
+	printf("Seraching in array: ");
+	for (i = L; i < R; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+}
+
+/**
+ * binary_search - find a value in array
+ * @array: input array of integers
+ * @size: lenght of array
+ * @value: looked value
+ *
+ * Description: find a value in array using the binary search algorithm
+ * Return: index where value is located or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int L, R, m;
+
+	if (array == NULL)
+		return (-1);
+
+	L = 0;
+	R = size - 1;
+
+	while (L <= R)
+	{
+		m = (L + R) / 2;
+		_print_binaryserach(array, L, R);
+		if (array[m] < value)
+			L = m + 1;
+		else if (array[m] > value)
+			R = m - 1;
+		else
+			return (m);
+	}
+
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void _print_binaryserach(int *array, int L, int R);
 #endif /* SEARCH_ALGTHM */
